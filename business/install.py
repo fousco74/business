a735c9e23a06690e47c82966c916dd9ab8b6f17e
konefousseni66@gmail.com <konefousseni66@gmail.com>
2026-06-10 17:43:42 +0000

    feat: workspacesidebarfixe

--- business/install.py
+++ business/install.py
@@ -1,4 +1,5 @@
 import frappe
+from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
 
 DEFAULT_PRINT_FORMATS = {
@@ -17,9 +18,106 @@ DEFAULT_PRINT_FORMATS = {
 	"Supplier Quotation": "Business Supplier Quotation",
 }
 
+# Custom fields required by Business print formats
+CUSTOM_FIELDS = {
+	"Company": [
+		{
+			"fieldname": "business_info_section",
+			"fieldtype": "Section Break",
+			"label": "Informations Business",
+			"insert_after": "tax_id",
+		},
+		{
+			"fieldname": "custom_rccm",
+			"fieldtype": "Data",
+			"label": "RCCM",
+			"insert_after": "business_info_section",
+		},
+		{
+			"fieldname": "custom_address_line",
+			"fieldtype": "Data",
+			"label": "Ligne d'adresse",
+			"insert_after": "custom_rccm",
+		},
+		{
+			"fieldname": "custom_capital_social",
+			"fieldtype": "Data",
+			"label": "Capital Social",
+			"insert_after": "custom_address_line",
+		},
+		{
+			"fieldname": "custom_bank_account_display",
+			"fieldtype": "Small Text",
+			"label": "Informations Bancaires",
+			"insert_after": "custom_capital_social",
+		},
+	],
+	"Supplier": [
+		{
+			"fieldname": "custom_rccm",
+			"fieldtype": "Data",
+			"label": "RCCM",
+			"insert_after": "tax_id",
+		},
+	],
+	"Delivery Note": [
+		{
+			"fieldname": "custom_observation",
+			"fieldtype": "Small Text",
+			"label": "Observations",
+			"insert_after": "terms",
+		},
+	],
+	"Material Request": [
+		{
+			"fieldname": "custom_priorite",
+			"fieldtype": "Select",
+			"label": "Priorité",
+			"options": "\nNormale\nHaute\nUrgente",
+			"insert_after": "schedule_date",
+		},
+		{
+			"fieldname": "custom_observation",
+			"fieldtype": "Small Text",
+			"label": "Observations",
+			"insert_after": "terms",
+		},
+	],
+	"Purchase Order": [
+		{
+			"fieldname": "custom_observation",
+			"fieldtype": "Small Text",
+			"label": "Observations",
+			"insert_after": "terms",
+		},
+	],
+	"Sales Invoice": [
+		{
+			"fieldname": "custom_observation",
+			"fieldtype": "Small Text",
+			"label": "Observations",
+			"insert_after": "terms",
+		},
+	],
+	"Stock Reconciliation": [
+		{
+			"fieldname": "custom_observation",
+			"fieldtype": "Small Text",
+			"label": "Observations",
+			"insert_after": "remarks",
+		},
+	],
+}
+
 
 def after_install():
+	create_business_custom_fields()
 	set_default_print_formats()
+
+
+def create_business_custom_fields():
+	create_custom_fields(CUSTOM_FIELDS, update=True)
+	frappe.db.commit()
 
 
 def set_default_print_formats():
